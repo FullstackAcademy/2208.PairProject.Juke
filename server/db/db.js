e739c78d67d64a499bc58d1473096580b9c53208
Benjamin Odisho <Semiroundpizza8@gmail.com>
2022-10-19 20:10:58 -0500

    bugfix

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5444/juke', {
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/juke', {
   logging: false
 })
 
